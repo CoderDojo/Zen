31268ffae8846398653c0579a997757b4dfcfb42
Ben Chapman <admin@dismiz.com>
2013-10-14 22:50:47 +0100

    Updated schema to latest

--- schema.sql
+++ schema.sql
@@ -1,6 +1,29 @@
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4096
+#
+# http://www.sequelpro.com/
+# http://code.google.com/p/sequel-pro/
+#
+# Host: nomorezen.dyndns.org (MySQL 5.5.24-0ubuntu0.12.04.1)
+# Database: db_1d8689d5
+# Generation Time: 2013-10-14 21:50:29 +0000
+# ************************************************************
 
-CREATE TABLE IF NOT EXISTS `ci_sessions` (
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table ci_sessions
+# ------------------------------------------------------------
+
+CREATE TABLE `ci_sessions` (
   `session_id` varchar(40) COLLATE utf8_bin NOT NULL DEFAULT '0',
   `ip_address` varchar(16) COLLATE utf8_bin NOT NULL DEFAULT '0',
   `user_agent` varchar(150) COLLATE utf8_bin NOT NULL,
@@ -9,12 +32,19 @@ CREATE TABLE IF NOT EXISTS `ci_sessions` (
   PRIMARY KEY (`session_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
-CREATE TABLE IF NOT EXISTS `dojos` (
+
+
+# Dump of table dojos
+# ------------------------------------------------------------
+
+CREATE TABLE `dojos` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(100) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `creator` int(20) NOT NULL,
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `verified` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `verified_at` timestamp NULL DEFAULT NULL,
+  `verified_by` int(20) DEFAULT NULL,
   `need_mentors` tinyint(1) unsigned NOT NULL DEFAULT '0',
   `stage` tinyint(1) unsigned NOT NULL DEFAULT '0',
   `time` varchar(100) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
@@ -23,23 +53,77 @@ CREATE TABLE IF NOT EXISTS `dojos` (
   `coordinates` varchar(50) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
   `notes` text CHARACTER SET utf8 COLLATE utf8_bin,
   `email` varchar(100) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `website` varchar(200) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
   `twitter` varchar(100) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
   `google_group` varchar(200) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
   `eb_id` varchar(25) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
   `supporter_image` varchar(200) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `deleted` tinyint(1) NOT NULL DEFAULT '0',
+  `deleted_by` int(11) DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `creator` (`creator`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=24 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE IF NOT EXISTS `login_attempts` (
+
+
+# Dump of table login_attempts
+# ------------------------------------------------------------
+
+CREATE TABLE `login_attempts` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `ip_address` varchar(40) COLLATE utf8_bin NOT NULL,
   `login` varchar(50) COLLATE utf8_bin NOT NULL,
   `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=6 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
-CREATE TABLE IF NOT EXISTS `users` (
+
+
+# Dump of table user_autologin
+# ------------------------------------------------------------
+
+CREATE TABLE `user_autologin` (
+  `key_id` char(32) COLLATE utf8_bin NOT NULL,
+  `user_id` int(11) NOT NULL DEFAULT '0',
+  `user_agent` varchar(150) COLLATE utf8_bin NOT NULL,
+  `last_ip` varchar(40) COLLATE utf8_bin NOT NULL,
+  `last_login` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`key_id`,`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+
+
+# Dump of table user_dojos
+# ------------------------------------------------------------
+
+CREATE TABLE `user_dojos` (
+  `user_id` int(11) NOT NULL DEFAULT '0',
+  `dojo_id` int(10) unsigned NOT NULL DEFAULT '0',
+  `owner` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`user_id`,`dojo_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table user_profiles
+# ------------------------------------------------------------
+
+CREATE TABLE `user_profiles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) unsigned NOT NULL,
+  `role` int(10) NOT NULL DEFAULT '1',
+  `dojo` int(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `dojo` (`dojo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+
+
+# Dump of table users
+# ------------------------------------------------------------
+
+CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(50) COLLATE utf8_bin NOT NULL,
   `password` varchar(255) COLLATE utf8_bin NOT NULL,
@@ -57,22 +141,14 @@ CREATE TABLE IF NOT EXISTS `users` (
   `created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=9 ;
-
-CREATE TABLE IF NOT EXISTS `user_autologin` (
-  `key_id` char(32) COLLATE utf8_bin NOT NULL,
-  `user_id` int(11) NOT NULL DEFAULT '0',
-  `user_agent` varchar(150) COLLATE utf8_bin NOT NULL,
-  `last_ip` varchar(40) COLLATE utf8_bin NOT NULL,
-  `last_login` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`key_id`,`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
-CREATE TABLE IF NOT EXISTS `user_profiles` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` int(10) unsigned NOT NULL,
-  `role` int(10) NOT NULL DEFAULT '1',
-  `dojo` int(20) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `dojo` (`dojo`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=7 ;
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
