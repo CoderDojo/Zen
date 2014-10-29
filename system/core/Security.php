4bd37060c62824036747c0ae8a9c3e92f2367315
Ben Chapman <hello@benchapman.ie>
2014-10-29 16:35:35 +0000

    Update to CodeIgniter, minor change to system/core/Config.php to avoid issues with PHP 5.6

--- system/core/Security.php
+++ system/core/Security.php
@@ -385,7 +385,7 @@ class CI_Security {
 
 		/*
 		 * Remove disallowed Javascript in links or img tags
-		 * We used to do some version comparisons and use of stripos for PHP5,
+		 * We used to do some version comparisons and use of stripos(),
 		 * but it is dog slow compared to these simplified non-capturing
 		 * preg_match(), especially if the pattern exists in the string
 		 */
@@ -509,8 +509,8 @@ class CI_Security {
 		}
 
 		$str = html_entity_decode($str, ENT_COMPAT, $charset);
-		$str = preg_replace('~&#x(0*[0-9a-f]{2,5})~i', 'chr(hexdec("\\1"))', $str);
-		return preg_replace('~&#([0-9]{2,4})~', 'chr(\\1)', $str);
+		$str = preg_replace('~&#x(0*[0-9a-f]{2,5})~ei', 'chr(hexdec("\\1"))', $str);
+		return preg_replace('~&#([0-9]{2,4})~e', 'chr(\\1)', $str);
 	}
 
 	// --------------------------------------------------------------------
